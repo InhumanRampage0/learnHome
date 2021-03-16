cbe02712e5bcac06f606c1e299894ca2244fbdd0
InhumanRampage0 <Boss4703264@gmail.com>
2021-03-16 17:54:15 +0300

    added selenide search test

---
+++ src/test/java/SeachTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class SeachTest {
+
+    @Test
+    void selenideSearchTest() {
+        open("https://www.google.com/");
+        $(byName("q")).setValue("Selenide").pressEnter();
+
+        $("#search").shouldHave(text("selenide.org"));
+    }
+
+
+
+
+
+}
